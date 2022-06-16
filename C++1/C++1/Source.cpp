183e1566263ce06d6166cfb1f1b0fa6c2044aa08
BolotovYevhen <bolotov_yevgeniy@ukr.net>
2022-06-16 16:07:41 +0300

    Translit (Russ-Eng)

--- C++1/C++1/Source.cpp
+++ C++1/C++1/Source.cpp
@@ -1,10 +1,92 @@
 #include <iostream>     //библиотеки
 #include <string>
+#include <fstream>
 
 using namespace std;
+
+ifstream fileIn1;
+ifstream fileIn2;
+ofstream fileOut;
+
+int readingFile(string &text)
+{
+    fileIn1.open("original.txt");
+
+    if (fileIn1.fail())
+    {
+        cout << "ERROR" << endl;
+        return 404;
+    }
+    while (!fileIn1.eof())
+    {
+        string str;
+        getline(fileIn1, str);
+        text += str;
+        text += '\n';
+    }
+    fileIn1.close();
+}
+
+int translitText(string text)
+{
+    string newText;
+    for (int i = 0; i < text.length(); i++)
+    {
+        fileIn2.open("lib.txt");
+
+        if (fileIn2.fail())
+        {
+            cout << "ERROR" << endl;
+            return 404;
+        }
+        bool kol = false;
+        while (!fileIn2.eof())
+        {
+            string str;
+            getline(fileIn2, str);
+            if (text[i] == str[0])
+            {
+                kol = true;
+                for (int j = 2; j < str.length(); j++)
+                {
+                    newText += str[j];
+                } 
+                break;
+            }
+            else if (text[i] + 32 == (int)str[0])
+            {
+                kol = true;
+                for (int j = 2; j < str.length(); j++)
+                {
+                    if (j == 2)
+                    {
+                        newText += str[j] - 32;
+                    }
+                    else
+                    {
+                        newText += str[j];
+                    }
+                }
+                break;
+            }
+        }
+        if (kol == false)
+        {
+            newText += text[i];
+        }
+        fileIn2.close();
+    }
+    fileOut.open("final.txt");
+    fileOut << newText;
+    fileOut.close();
+
+}
 
 void main()
 {
     setlocale(LC_ALL, "Russian"); //руссификатор
-    cout << "Привет" << endl;
+    string text = "";
+    readingFile(text);
+    cout << text;
+    translitText(text);
 }
