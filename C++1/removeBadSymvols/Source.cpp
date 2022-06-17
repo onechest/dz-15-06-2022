b95bb4e7e3f612e3263e335db81c74d909e375df
BolotovYevhen <bolotov_yevgeniy@ukr.net>
2022-06-17 09:20:54 +0300

    add

---
+++ C++1/removeBadSymvols/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>     //библиотеки
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+ifstream fileIn;
+ofstream fileOut;
+
+
+void main()
+{
+    setlocale(LC_ALL, "Russian"); //руссификатор
+    
+}
